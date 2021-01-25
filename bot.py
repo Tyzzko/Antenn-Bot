277b0301cc5bcb64e52b2cb9bb01c65a69fbd0e2
Tyzzko <70973519+Tyzzko@users.noreply.github.com>
2021-01-25 13:30:18 +0300

    commit1

--- bot.py
+++ bot.py
@@ -4,42 +4,55 @@ from aiogram.utils import executor
 from pyorbital import tlefile
 from datetime import datetime
 from pyorbital.orbital import Orbital
+from requests import get
 
 from config import TOKEN
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+def update_tle():
+    file = open("tle.txt", "ab")
+    file.write(get("http://www.celestrak.com/NORAD/elements/weather.txt").content)                      #
+    # print("get tle successfull")
+    file.close()
 
-tle_noaa18 = tlefile.read('NOAA 18', 'C:\Program Files (x86)\Orbitron\Tle\weather')
-tle_noaa19 = tlefile.read('NOAA 19', 'C:\Program Files (x86)\Orbitron\Tle\weather')
-tle_noaa15 = tlefile.read('NOAA 15', 'C:\Program Files (x86)\Orbitron\Tle\weather')
-tle_meteor_m2 = tlefile.read('METEOR-M2', 'C:\Program Files (x86)\Orbitron\Tle\weather')
+# bot = Bot(token=TOKEN)
+# dp = Dispatcher(bot)
 
-orb = Orbital("Russia NPP")
+tle_file = update_tle()
+
+tle_noaa18 = tlefile.read('NOAA 18', tle_file)
+tle_noaa19 = tlefile.read('NOAA 19', tle_file)
+tle_noaa15 = tlefile.read('NOAA 15', tle_file)
+tle_meteor_m2 = tlefile.read('METEOR-M 2', tle_file)
+
+orb = Orbital()
 now = datetime.utcnow()
 lon, lat, alt = Orbital.get_lonlatalt(now)
-passes = {'noaa15': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
+
+
+passes = {'noaa15': Orbital.get_next_passes(datetime.time(), 1, lon, lat, alt, tol=0.001, horizon=0),
           'noaa18': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
           'noaa19': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
           'meteor_m2': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0)}
 
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+print(passes)
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
-
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
+# @dp.message_handler(commands=['start'])
+# async def process_start_command(message: types.Message):
+#     await message.reply("Привет!\nНапиши мне что-нибудь!")
+#
+#
+# @dp.message_handler(commands=['help'])
+# async def process_help_command(message: types.Message):
+#     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+#
+#
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text)
+#
+#
+# if __name__ == '__main__':
+#     executor.start_polling(dp)
 
 # print(help(pyorbital))
