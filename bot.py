d29f85d0c8e80c33ea46b23d3fc00ae4aad84e83
Tyzzko <70973519+Tyzzko@users.noreply.github.com>
2021-01-25 12:09:26 +0300

    sat

--- bot.py
+++ bot.py
@@ -1,13 +1,27 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import pyorbital
+from pyorbital import tlefile
+from datetime import datetime
+from pyorbital.orbital import Orbital
 
 from config import TOKEN
 
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+tle_noaa18 = tlefile.read('NOAA 18', 'C:\Program Files (x86)\Orbitron\Tle\weather')
+tle_noaa19 = tlefile.read('NOAA 19', 'C:\Program Files (x86)\Orbitron\Tle\weather')
+tle_noaa15 = tlefile.read('NOAA 15', 'C:\Program Files (x86)\Orbitron\Tle\weather')
+tle_meteor_m2 = tlefile.read('METEOR-M2', 'C:\Program Files (x86)\Orbitron\Tle\weather')
+
+orb = Orbital("Russia NPP")
+now = datetime.utcnow()
+lon, lat, alt = Orbital.get_lonlatalt(now)
+passes = {'noaa15': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
+          'noaa18': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
+          'noaa19': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0),
+          'meteor_m2': Orbital.get_next_passes(datetime.time(), 24, lon, lat, alt, tol=0.001, horizon=0)}
 
 
 @dp.message_handler(commands=['start'])
@@ -29,4 +43,3 @@ if __name__ == '__main__':
     executor.start_polling(dp)
 
 # print(help(pyorbital))
-
