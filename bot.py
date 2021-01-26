1049f757e927281e219327c86f5cb899db6b3aa0
Yarik <lynx9001@yandex.ru>
2021-01-26 22:54:11 +0300

    aaaaaaaaaaaaaaaaaaa

--- bot.py
+++ bot.py
@@ -9,7 +9,11 @@ from pyorbital.orbital import Orbital
 from requests import get
 
 from config import TOKEN
-hour = 72
+hou = 24
+pitch = 45
+long_poz = 43.8399 
+lat_poz= 55.3948
+height_poz = 163
 
 def update_tle():
     file = open("tle.txt", "ab")
@@ -34,16 +38,18 @@ tle_noaa15 = tlefile.read('NOAA 15', tle_file)
 tle_meteor_m2 = tlefile.read('METEOR-M 2', tle_file)
 '''
 utc_time = datetime.utcnow()
+'''
 noaa_15_lon, noaa_15_lat, noaa_15_alt = noaa_15.get_lonlatalt(utc_time)
 noaa_18_lon, noaa_18_lat, noaa_18_alt = noaa_18.get_lonlatalt(utc_time)
 noaa_19_lon, noaa_19_lat, noaa_19_alt = noaa_19.get_lonlatalt(utc_time)
 meteor_m2_lon, meteor_m2_lat, meteor_m2_alt = meteor_m2.get_lonlatalt(utc_time)
-
-passes = {'noaa15': noaa_15.get_next_passes(utc_time, hour, noaa_15_lon, noaa_15_lat, noaa_15_alt, tol=0.001, horizon=0),
-          'noaa18': noaa_18.get_next_passes(utc_time, hour, noaa_18_lon, noaa_18_lat, noaa_18_alt, tol=0.001, horizon=0),
-          'noaa19': noaa_19.get_next_passes(utc_time, hour, noaa_19_lon, noaa_19_lat, noaa_19_alt, tol=0.001, horizon=0),
-          'meteor_m2': meteor_m2.get_next_passes(utc_time, hour, meteor_m2_lon, meteor_m2_lat,
-                                                 meteor_m2_alt, tol=0.001, horizon=0)}
+'''
+print(utc_time)
+passes = {'noaa15': noaa_15.get_next_passes(utc_time, hou, long_poz, lat_poz, height_poz, tol=0.001, horizon=pitch),
+          'noaa18': noaa_18.get_next_passes(utc_time, hou, long_poz, lat_poz, height_poz, tol=0.001, horizon=pitch),
+          'noaa19': noaa_19.get_next_passes(utc_time, hou, long_poz, lat_poz, height_poz, tol=0.001, horizon=pitch),
+          'meteor_m2': meteor_m2.get_next_passes(utc_time, hou, long_poz, lat_poz,
+                                                 height_poz, tol=0.001, horizon=pitch)}
 print(passes)
 
 
