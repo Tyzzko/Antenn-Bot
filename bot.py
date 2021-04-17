f952f8d67765d720dce2f3685d9b54fd90620800
fgupo <f.g.uporot@gmail.com>
2021-04-17 20:02:13 +0300

    какая-то ересь

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from main import satellite
+from datetime import timedelta
 from config import TOKEN
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram import Bot, Dispatcher, executor
-import sqlite3
 from datetime import datetime
+from main import passes
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,12 +14,19 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['help'])
+@dp.message_handler()
 async def send_photos(message: types.Message):
-    for i in satellite.current_photos:
-        path = f'C:/cartinki/cartinki/mcir/{i}'
-        cap = f'{satellite.current_photos[0].split("-"[:2]), datetime.now()}'
-        await bot.send_photo(message.chat.id, path, cap)
+    for _ in range(1):          #satellite.current_photos
+        positively = 0
+        for i in passes:
+            for j in passes[i]:
+                while j[2] + timedelta(minutes=2) != datetime.now():
+                    positively = 1
+                if positively != 0:
+                    await bot.send_message(message.chat.id, 'есть контакт!')
+        # path = f'C:/cartinki/cartinki/mcir/{i}'
+        # cap = f'{satellite.current_photos[0].split("-"[:2]), datetime.now()}'
+        # await bot.send_photo(message.chat.id, path, cap)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta  # импорт внешних модулей
-import dp as dp
-import asyncio
-from aiogram.types import message
+# import dp as dp
 from pyorbital.orbital import Orbital
 from requests import get
 import sqlite3
@@ -15,7 +13,7 @@ def update_tle():  # функция обнавления tle файлов
 
 
 class Satellite_Bot:
-    hou = 24  # кол-во часов для расчета пролетов
+    hour = 24  # кол-во часов для расчета пролетов
     pitch = 11  # мин высота
     long_poz = 43.8399  # долгота широта высота
     lat_poz = 55.3948
@@ -24,8 +22,6 @@ class Satellite_Bot:
 
     # Функция для расчета пролетов для спутников работающих в укв диапазоне
     def calculation_satellite(self):
-        ###
-        # тут надо написать условие обнавления tle (померить время и если разница больше чем три дня то обнавляем)
         if Satellite_Bot.update_time == 0 or \
                 str((Satellite_Bot.update_time - (datetime.date(datetime.now())))).split('-')[-1] == '03':
             Satellite_Bot.update_time = (datetime.date(datetime.now()))
@@ -35,19 +31,24 @@ class Satellite_Bot:
         self.noaa_19 = Orbital("NOAA-19", tle_file='tle.txt')
         self.noaa_15 = Orbital("NOAA-15", tle_file='tle.txt')
         self.meteor_m2 = Orbital("METEOR-M 2", tle_file='tle.txt')
+        self.meteor_m2_2 = Orbital("METEOR-M2 2", tle_file='tle.txt')
 
-        self.plans_noaa15 = self.noaa_15.get_next_passes(self.utc_time, Satellite_Bot.hou, Satellite_Bot.long_poz,
+        self.plans_noaa15 = self.noaa_15.get_next_passes(self.utc_time, Satellite_Bot.hour, Satellite_Bot.long_poz,
                                                          # расчет пролетов
                                                          Satellite_Bot.lat_poz, Satellite_Bot.height_poz, tol=0.001,
                                                          horizon=Satellite_Bot.pitch)
-        self.plans_noaa18 = self.noaa_18.get_next_passes(self.utc_time, Satellite_Bot.hou,
+        self.plans_noaa18 = self.noaa_18.get_next_passes(self.utc_time, Satellite_Bot.hour,
                                                          Satellite_Bot.long_poz, Satellite_Bot.lat_poz,
                                                          Satellite_Bot.height_poz, tol=0.001,
                                                          horizon=Satellite_Bot.pitch)
-        self.plans_noaa19 = self.noaa_19.get_next_passes(self.utc_time, Satellite_Bot.hou, Satellite_Bot.long_poz,
+        self.plans_noaa19 = self.noaa_19.get_next_passes(self.utc_time, Satellite_Bot.hour, Satellite_Bot.long_poz,
                                                          Satellite_Bot.lat_poz, Satellite_Bot.height_poz, tol=0.001,
                                                          horizon=Satellite_Bot.pitch)
-        self.plans_meteor_m2 = self.meteor_m2.get_next_passes(self.utc_time, Satellite_Bot.hou, Satellite_Bot.long_poz,
+        self.plans_meteor_m2 = self.meteor_m2.get_next_passes(self.utc_time, Satellite_Bot.hour, Satellite_Bot.long_poz,
+                                                              Satellite_Bot.lat_poz, Satellite_Bot.height_poz,
+                                                              tol=0.001,
+                                                              horizon=Satellite_Bot.pitch)
+        self.plans_meteor_m2_2 = self.meteor_m2_2.get_next_passes(self.utc_time, Satellite_Bot.hour, Satellite_Bot.long_poz,
                                                               Satellite_Bot.lat_poz, Satellite_Bot.height_poz,
                                                               tol=0.001,
                                                               horizon=Satellite_Bot.pitch)
@@ -55,7 +56,8 @@ class Satellite_Bot:
         self.passes = {'noaa15': self.plans_noaa15,  # конфигурация словаря
                        'noaa18': self.plans_noaa18,
                        'noaa19': self.plans_noaa19,
-                       'meteor_m2': self.plans_meteor_m2}
+                       'meteor_m2': self.plans_meteor_m2,
+                       'meteor_m2_2': self.plans_meteor_m2_2}
         return self.passes  # Возврат словоря спутник: список пролетов
 
     def plans_txt_list(self, passes_slovar: dict):
@@ -82,20 +84,26 @@ class Satellite_Bot:
     def get_current_photos(self, list):
         self.current_photos = list
 
-satellite = Satellite_Bot()
 
+satellite = Satellite_Bot()
+positively = 0
 passes = satellite.calculation_satellite()
-# print(passes)
 satellite.plans_txt_list(satellite.calculation_satellite())
-for i in passes:
-    for j in passes[i]:
-        if j[1] + timedelta(minutes=5) == datetime.now():
-            con = sqlite3.connect('data_base.sql')
-            cur = con.cursor()
-            result = cur.execute("""SELECT file_name FROM photos
-                        WHERE year = 2010""").fetchall()
-            current_files = os.listdir('C:/Program Files (x86)/WXtoImg/images')
-            satellite.get_current_photos(list(set(current_files).intersection(set(result))))
-            for k in satellite.current_photos:
-                cur.execute(f'INSERT INTO photos file_name {k}')
-            print(satellite.current_photos)
+print('\n'.join(f'{i}: {passes[i]}' for i in passes))
+print(datetime.utcnow())
+satellite.plans_txt_list(satellite.calculation_satellite())
+# for i in passes:
+#     for j in passes[i]:
+#         if j[0] + timedelta(minutes=5) == datetime.utcnow():
+#             positively = 1
+#             # con = sqlite3.connect('data_base.sql')
+#             # cur = con.cursor()
+#             # result = cur.execute("""SELECT file_name FROM photos
+#             #             WHERE year = 2010""").fetchall()
+#             # current_files = os.listdir('C:/Program Files (x86)/WXtoImg/images')
+#             # satellite.get_current_photos(list(set(current_files).intersection(set(result))))
+#             # for k in satellite.current_photos:
+#             #     cur.execute(f'INSERT INTO photos file_name {k}')
+#             # print(satellite.current_photos)
+#             positively = 0
+#
