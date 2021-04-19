3a016a3cb151b131f613e1a4b19b154028645f72
fgupo <f.g.uporot@gmail.com>
2021-04-19 15:43:46 +0300

    че-то изменилось

--- bot.py
+++ bot.py
@@ -1,49 +1,43 @@
-from datetime import timedelta
 from config import TOKEN
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram import Bot, Dispatcher, executor
-from datetime import datetime
-from main import passes
+from datetime import datetime, timedelta
+from main import passes, satellite
+import os
+import sqlite3
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler()
 async def send_photos(message: types.Message):
-    # satellite.current_photos
-    for i in passes:
-        for j in passes[i]:
-            while j[2] + timedelta(minutes=2) != datetime.now():
-                await bot.send_message(message.chat.id, 'есть контакт!')
-                break
-            break
-            # path = f'C:/Program Files (x86)/WXtoImg/Images/"{i}'
-            # day, month, minute, second = str(j[0].day), str(j[0].month), str(j[0].minute), str(j[0].second)
-            # if len(day) == 1:
-            #     day = '0' + day
-            # if len(month) == 1:
-            #     month = '0' + month
-            # if len(minute) == 1:
-            #     minute = '0' + minute
-            # if len(second) == 1:
-            #     second = '0' + second
-            # path += f'-{month}{day}{minute}{second}"'
-            # con = sqlite3.connect('data_base.sql')
-            # cur = con.cursor()
-            # result = cur.execute("""SELECT file_name FROM photos
-            #             WHERE year = 2010""").fetchall()
-            # current_files = os.listdir('C:/Program Files (x86)/WXtoImg/images')
-            # satellite.get_current_photos(list(set(current_files).intersection(set(result))))
-            # for k in satellite.current_photos:
-            #     cur.execute(f'INSERT INTO photos file_name {k}')
-            # cap = f'{satellite.current_photos[0].split("-"[:2]), datetime.now()}'
-            # await bot.send_photo(message.chat.id, path, cap)
+    while True:
+        for i in passes:
+            for j in passes[i]:
+                if j[1] + timedelta(minutes=2) == datetime.now():
+                    day, month, minute, second = str(j[0].day), str(j[0].month), str(j[0].minute), str(j[0].second)
+                    if len(day) == 1:
+                        day = '0' + day
+                    if len(month) == 1:
+                        month = '0' + month
+                    if len(minute) == 1:
+                        minute = '0' + minute
+                    if len(second) == 1:
+                        second = '0' + second
+                    path = f'C:/Program Files (x86)/WXtoImg/Images/"{i}-{month}{day}{minute}{second}"'
+                    con = sqlite3.connect('data_base.sql')
+                    cur = con.cursor()
+                    sql_req = f'SELECT file_name FROM photos WHERE file_name = "{i}-{month}{day}{minute}{second}"'
+                    result = cur.execute(sql_req).fetchall()
+                    current_files = os.listdir('C:/Program Files (x86)/WXtoImg/imagels')
+                    file = list(set(current_files).intersection(set(result)))
+                    cur.execute(f'INSERT INTO photos file_name {file[0]}')
+                    await bot.send_message(message.chat.id, j)
+                    await bot.send_photo(message.chat.id, path, file[0])
+                else:
+                    await bot.send_message(message.chat.id, 'спутников нема(')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from datetime import datetime, timedelta  # импорт внешних модулей
-# import dp as dp
+from datetime import datetime
 from pyorbital.orbital import Orbital
 from requests import get
-import sqlite3
-import os
 
 
-def update_tle():  # функция обнавления tle файлов
+def update_tle():
     file = open("tle.txt", "ab")
     file.write(get("http://www.celestrak.com/NORAD/elements/weather.txt").content)
     file.close()
 
 
 class Satellite_Bot:
-    hour = 24  # кол-во часов для расчета пролетов
-    pitch = 11  # мин высота
-    long_poz = 43.8399  # долгота широта высота
+    hour = 24
+    pitch = 11
+    long_poz = 43.8399 
     lat_poz = 55.3948
     height_poz = 0
     update_time = 0
@@ -61,35 +58,32 @@ class Satellite_Bot:
                        'meteor_m2_2': self.plans_meteor_m2_2}
         return self.passes  # Возврат словоря спутник: список пролетов
 
-    def plans_txt_list(self, passes_slovar: dict):
-        mass = ['noaa-15', 'noaa-18', 'noaa-19', 'meteor_m2', 'meteor_m2_2']
-        self.plans = open('plans_1.txt', 'w')
-        for n in mass:
-            for i in passes_slovar[n]:
+    def plans_txt_list(self, passes_dict):
+        arr = ['noaa-15', 'noaa-18', 'noaa-19', 'meteor_m2', 'meteor_m2_2']
+        self.plans = open('plans.txt', 'w')
+        for n in arr:
+            for i in passes_dict[n]:
                 self.plans.write(f'{n}\n')
                 for a in i:
                     self.plans.write(f'{a}\n')
                 self.plans.write('----------\n')
 
-    def loger_start_proger(self):  # Логирование запуска
+    def loger_start_proger(self):
         self.log = open('log.txt', 'a+')
         self.log.write('Programm_Start: ' + str(datetime.today())[:-7] + '\n')
         self.log.close()
 
-    def loger_stop_proger(self):  # Логирование завершения работы
+    def loger_stop_proger(self):
         self.log = open('log.txt', 'a+')
         self.log.write('Programm_Stop: ' + str(datetime.today())[:-7] + '\n')
         self.log.write('----------\n')
         self.log.close()
-
-    def get_current_photos(self, list):
-        self.current_photos = list
+    #
+    # def get_current_photos(self, list):
+    #     self.current_photos = list
 
 
 satellite = Satellite_Bot()
 passes = satellite.calculation_satellite()
-satellite.plans_txt_list(satellite.calculation_satellite())
+satellite.plans_txt_list(passes)
 print('\n'.join(f'{i}: {passes[i]}' for i in passes))
-satellite.plans_txt_list(satellite.calculation_satellite())
-
-
